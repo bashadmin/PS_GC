c295f1446f3cc0707a3f498784e4eff2f802362c
Yonatan Arnold <dev.arnold0@gmail.com>
2021-02-20 18:27:15 -0500

    Adding names of who is in the Group 3 on 20 (name correction spelling) Feb. 2021 - Yonatan Arnold

--- SR3SI.py
+++ SR3SI.py
@@ -1,5 +1,5 @@
 # Testing the code!
 
-# Group 3 for 20 Feb. 2021: Basaddutta Das, Luke Stamm, Melody, Yosha, Yonatan
+# Group 3 for 20 Feb. 2021: Basabdutta Das, Luke Stamm, Melody, Yosha, Yonatan
 
 print("Hello World")
