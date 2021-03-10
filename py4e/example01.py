011068fb4ad36ebd40ef33566f8c4e75f0153e0c
bashadmin <dev.sugarman@gmail.com>
2021-03-09 23:43:03 -0500

    added py4e folder and first lesson

---
+++ py4e/example01.py
@@ -0,0 +1,19 @@
+name = input('Enter file:')
+handle = open(name, 'r')
+counts = dict()
+
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+bigcount = None
+bigword = None
+for word, count in list(counts.items()):
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
+print(bigword, bigcount)
+
+# Code: http://www.py4e.com/code3/words.py
